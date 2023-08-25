f23b615ae90c5b436fde08fafaa1b271cbaeb932
Matt Enyeart <menyeart1@gmail.com>
2023-08-24 18:02:53 -0600

    Complete iteration 3

--- reunion/reunion.rb
+++ reunion/reunion.rb
@@ -9,4 +9,27 @@ class Reunion
     @activities << activity
   end
 
+  def total_cost
+    @activities.sum { |activity| activity.total_cost}
+  end
+
+  def total_owed
+    total_owed = Hash.new(0)
+    @activities.each do |activity|
+      activity.owed.each do |k,v|
+        total_owed[k] += v
+      end
+    end
+    total_owed
+  end
+
+  def print_summary
+    final_string = ""
+    total_owed.each do |person|
+      binding.pry
+      final_string.concat("#{person[0]} owes #{person[1]}$\n") 
+    end
+    final_string
+  end
+
 end
